9749e3f70c800be0838d00f7dfbc42365cc75487
Hileene <92981899+Hileene@users.noreply.github.com>
2025-09-27 16:35:47 +0200

    Refactor database connection to use environment variables for credentials

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,5 +1,6 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
-const connection = mongoose.connect("mongodb+srv://khansohail015:crudapp@cluster0.tdxufn4.mongodb.net/AllUserData?retryWrites=true&w=majority")
+const connection = `mongoose.connect("mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vs4lxeu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0")`
 const userSchema = mongoose.Schema(
     {
         name: String,
